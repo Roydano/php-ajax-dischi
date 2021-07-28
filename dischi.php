cf0a81c3f7b6e804a838d6ccc55d0874f882110c
Roydano <roy.dano90@gmail.com>
2021-07-28 14:59:22 +0200

    add milestone 1 con leggera formattazione su css

--- dischi.php
+++ dischi.php
@@ -1,12 +1,35 @@
+<?php 
+    include 'lista-dischi.php'
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Dischi</title>
 </head>
 <body>
+    <div class="container">
+        <?php foreach($dischi as $key => $music) { ?>
+            <div class="card">
+
+                <div class="img_cover">
+                    <img src="<?php echo $music['poster'] ?>" alt="<?php echo $music['title'] ?>">
+                </div>
+                <h3><?php echo $music['title'] ?></h3>
+                <p><?php echo $music['author'] ?></p>
+                <p><?php echo $music['year'] ?></p>
+
+            </div>
+
+
+        <?php } ?>
+    </div>
     
 </body>
 </html>
---
+++ lista-dischi.php
@@ -0,0 +1,92 @@
+<?php 
+
+    $dischi = [
+        'disco1' => [
+            "poster" => "https:\/\/www.onstageweb.com\/wp-content\/uploads\/2018\/09\/bon-jovi-new-jersey.jpg",
+            "title" => "New Jersey",
+            "author" => "Bon Jovi",
+            "genre" => "Rock",
+            "year" => "1988"
+        ],
+
+        'disco2' => [
+            "poster" => "https://images-na.ssl-images-amazon.com/images/I/51FPNBYSmZL._AC_.jpg",
+            "title" => "Live at Wembley 86",
+            "author" => "Queen",
+            "genre" => "Rock",
+            "year" => "1992"
+        ],
+
+        'disco3' => [
+            "poster" => "https://images-eu.ssl-images-amazon.com/images/I/41JD3CW65HL.__AC_QL70_ML2_.jpg",
+            "title" => "Ten's Summoner's Tales",
+            "author" => "Sting",
+            "genre" => "Pop",
+            "year" => "1993"
+        ],
+
+        'disco4' => [
+            "poster" => "https:\/\/cdn2.jazztimes.com\/2018\/05\/SteveGadd-800x723.jpg",
+            "title" => "Steve Gadd Band",
+            "author" => "Steve Gadd Band",
+            "genre" => "Jazz",
+            "year" => "2018"
+        ],
+
+        'disco5' => [
+            "poster" => "https:\/\/images-na.ssl-images-amazon.com\/images\/I\/810nSIQOLiL._SY355_.jpg",
+            "title" => "Brave new World",
+            "author" => "Iron Maiden",
+            "genre" => "Metal",
+            "year" => "2000"
+        ],
+
+        'disco6' => [
+            "poster" => "https:\/\/upload.wikimedia.org\/wikipedia\/en\/9\/97\/Eric_Clapton_OMCOMR.jpg",
+            "title" => "One more car, one more raider",
+            "author" => "Eric Clapton",
+            "genre" => "Rock",
+            "year" => "2002"
+        ],
+
+        'disco7' => [
+            "poster" => "https:\/\/images-na.ssl-images-amazon.com\/images\/I\/51rggtPgmRL.jpg",
+            "title" => "Made in Japan",
+            "author" => "Deep Purple",
+            "genre" => "Rock",
+            "year" => "1972"
+        ],
+
+        'disco8' => [
+            "poster" => "https://i.pinimg.com/originals/62/c6/e0/62c6e0b44d1ddd7d3a1b3dd371aca2e2.jpg",
+            "title" => "And Justice for All",
+            "author" => "Metallica",
+            "genre" => "Metal",
+            "year" => "1988"
+        ],
+
+        'disco9' => [
+            "poster" => "https://images-na.ssl-images-amazon.com/images/I/71wvKUYZBcL._AC_SL1000_.jpg",
+            "title" => "Hard Wired",
+            "author" => "Dave Weckl",
+            "genre" => "Jazz",
+            "year" => "1994"
+        ],
+
+        'disco10' => [
+            "poster" => "https://images-na.ssl-images-amazon.com/images/I/6148jblbNqL._AC_SL1500_.jpg",
+            "title" => "Bad",
+            "author" => "Michael Jackson",
+            "genre" => "Pop",
+            "year" => "1987"
+        ],
+
+    ];
+
+
+
+
+
+
+
+?>
